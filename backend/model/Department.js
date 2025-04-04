21cb263f63ddf0aae8c7c0d623e22706d0e60de1
joelharris181106 <joel.harris@kalvium.community>
2025-04-04 15:40:35 +0530

    schema done

--- backend/model/Department.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const departmentSchema = new mongoose.Schema({
-    departmentName: { type: String, required: true },
-    services: [{ type: mongoose.Schema.Types.ObjectId, ref: "Service" }]
-});
-
-module.exports = mongoose.model("Department", departmentSchema);
